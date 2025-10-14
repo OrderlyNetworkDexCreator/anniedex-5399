f6b40c9183ed855d8ecaac54e7b028a2505ec46e
Mario Reder <mario.reder@pm.me>
2025-10-14 08:23:32 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -17,7 +17,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "A powerful Dex powered by Orderly & Annie",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio,Leaderboard,Rewards",
+  "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio,Leaderboard,Rewards,Swap,Vaults",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/Orderly_Annie",
